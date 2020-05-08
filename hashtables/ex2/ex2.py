3183a0b97cc13d6e8ebed7da1044b3e32b186b71
Jessica Cooper <jessicagcooper@gmail.com>
2020-05-08 13:09:04 -0500

    Completed ex2.py. Passes tests. Linear time complexity.

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -7,8 +7,14 @@ class Ticket:
 
 def reconstruct_trip(tickets, length):
     
-    """
-    YOUR CODE HERE
-    """
+    t = dict()
 
+    for ticket in tickets:
+        t[ticket.source] = ticket.destination
+
+    route = []
+    route.append(t['NONE'])
+    for i in route:
+        if i in t and len(route) < length:
+            route.append(t[i])
     return route
