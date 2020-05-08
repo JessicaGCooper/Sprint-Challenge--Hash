16bbe51028346155dd0dda4fdbe3941331e3eff3
Jessica Cooper <jessicagcooper@gmail.com>
2020-05-08 12:16:18 -0500

    Completed ex4.py. Passing tests.

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,9 +1,20 @@
 def has_negatives(a):
 
-    """
-    YOUR CODE HERE
-    """
-
+    no_negatives = []
+    for each in a:
+        no_negatives.append(abs(each))
+    
+    check = dict()
+    for num in no_negatives:
+        if num not in check:
+            check[num] = 1
+        else:
+            check[num] += 1
+    result = []
+    for num, count in check.items():
+        if count > 1:
+            result.append(num)
+    
     return result
 
 
